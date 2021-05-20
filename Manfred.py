313903b96808594173af1af18fb192212e2b2582
F0X0000 <76114016+F0X0000@users.noreply.github.com>
2021-05-20 14:44:27 +0200

    Revert "Revert "Manfred 0.1""
    This reverts commit 729371efb70796470ab42f6fe1ad358d0d79a274.

---
+++ Manfred.py
@@ -0,0 +1,97 @@
+###########################Manfred##############################################
+from skript import *
+############################install#############################################
+setup()
+
+import os
+from audio import get_audio
+from gtts import gTTS
+import speech_recognition as sr
+import playsound
+import time
+import sys
+import random
+from say import  *
+from logo import logo
+from time import sleep
+from pogoda import *
+from czy import *
+import webbrowser
+
+################################################################################
+
+logo()
+say("Witaj, Jestem Manfred")
+
+################################################################################
+
+yt = ["youtube", "youtubie", "film"]
+ADMIN_COMMADN = ["admin"]
+PA_ADMIN = ["pa", "pa pa", "papa"]
+RESTART_ADMIN = ["restart"]
+KLUCZ_COMMAND = ["ok", "manfred", "bot", "okej", "żulu",  "manfredi"]
+PROSZE = ["prosze", "proszę"]
+odpal = ["odpal"]
+google = ["google"]
+jaka = ["jaka"]
+jest = ["jest"]
+setupup = ["satup"]
+temperatura = ["temperatura"]
+w = ["w"]
+BIAŁYSTOK = ["białymstoku"]
+BIELSK = ["bielsk", "bielsko"]
+bielskbiałej = ["bielsk-białej", "bielsku-białej"]
+BIAŁE = ["białe"]
+WARSZAWA = ["warszawie"]
+wyszukaj = ["wyszukaj"]
+
+################################################################################
+
+################################################################################
+
+def COMMAND():
+    while True:
+        print(" ")
+        audio = get_audio()
+        if audio != None:
+            if len(czy(audio, ADMIN_COMMADN)):
+                if len(czy(audio, google)):
+                    g = audio.lower().split(' ' + czy(audio, google)[0] + ' ')[1]
+                    say("Oto co mi się znaleść.")
+                    googleurl = "https://www.google.com/search?q=" + g.replace(" ", "+").replace("?", "%3F")
+                    webbrowser.open(googleurl)
+                if len(czy(audio, PA_ADMIN)):
+                    say("pa")
+                    quit()
+                    exit()
+                if len(czy(audio, RESTART_ADMIN)):
+                    os.system("cls")
+                    os.system("python Manfred.py")
+                if len(czy())
+            if len(czy(audio, KLUCZ_COMMAND)):
+                if len(czy(audio, jaka)):
+                    if len(czy(audio, jest)):
+                        if len(czy(audio, temperatura)):
+                            if len(czy(audio, w)):
+                                if len(czy(audio, BIAŁYSTOK)):
+                                    temp("Białystok")
+                                if len(czy(audio, WARSZAWA)):
+                                    temp("Warszawa")
+                                if len(czy(audio, bielskbiałej)):
+                                    pogo("Bielsko")
+                                if len(czy(audio, BIELSK)):
+                                    if len(czy(audio, BIAŁE)):
+                                        pogo("Bielsko Biała")
+
+                if len(czy(audio, wyszukaj)):
+                    g = audio.lower().split(' ' + czy(audio, wyszukaj)[0] + ' ')[1]
+                    say("Oto co mi się udało znaleść.")
+                    googleurl = "https://www.google.com/search?q=" + g.replace(" ", "+").replace("?", "%3F")
+                    webbrowser.open(googleurl)
+                if len(czy(audio, odpal)):
+                    if len(czy(audio, PROSZE)):
+                        if len(czy(audio, yt)):
+                            ytw = audio.lower().split(' ' + czy(audio, yt)[0] + ' ')[1]
+                            youtubeopen(ytw)
+
+COMMAND()
---
+++ skript.py
@@ -0,0 +1,36 @@
+def setup():
+    import os
+    r = ["t"]
+    f = open("install.txt", "r+")
+    fr = f.read(1)
+    if fr in r:
+        print(" ")
+    else:
+        os.system("PyAudio-0.2.11-cp39-cp39-win_amd64.whl")
+        os.system("pip install SpeechRecognition")
+        os.system("pip install PyAudio")
+        os.system("pip install playsound")
+        os.system("pip install gTTS")
+        os.system("cls")
+        f.write("t")
+        f.close()
+
+def youtubeopen(text):
+    import urllib.request
+    import re
+    import webbrowser
+    import say
+
+    say.say("odpalam")
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + text)
+    video = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    webbrowser.open("https://www.youtube.com/watch?v=" + video[0])
+
+def satupadd():
+    import os
+    os.system("PyAudio-0.2.11-cp39-cp39-win_amd64.whl")
+    os.system("pip install SpeechRecognition")
+    os.system("pip install PyAudio")
+    os.system("pip install playsound")
+    os.system("pip install gTTS")
+    os.system("cls")
