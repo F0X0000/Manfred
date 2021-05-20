2314618074ad4d192d176a5bd054f885fc7bad43
F0X0000 <76114016+F0X0000@users.noreply.github.com>
2021-05-20 14:38:53 +0200

    Manfred 0.1

---
+++ audio.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Nasłuchuję...")
+        audio = r.listen(source)
+        said=""
+        try:
+            said= r.recognize_google(audio, language="pl")
+            print("Powiedziałeś: " + said.lower())
+        except Exception as e:
+            print("Czekam na komendę..." + str(e))
+
+    return said.lower()
---
+++ say.py
@@ -0,0 +1,15 @@
+import playsound
+import os
+from gtts import gTTS
+
+def say(text):
+    tts = gTTS(text=text, lang="pl")
+    filename = "voice.mp3"
+    tts.save(filename)
+    print(text)
+    playsound.playsound(filename)
+    os.remove('voice.mp3')
+
+def lossay(text):
+    a = random.choice(text)
+    say(a)
